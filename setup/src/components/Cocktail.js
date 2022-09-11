5bd41913201e03d70c00242dfdb18d3e2f3f8ebe
bing yang <bingyang9258@gmail.com>
2022-09-11 12:59:53 -0400

    fetch api data and display cocktail list

--- setup/src/components/Cocktail.js
+++ setup/src/components/Cocktail.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cocktail = () => {
+const Cocktail = (cocktail) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
+    <article className='cocktail'>
+      <div className='img-container'>
+        <img src={cocktail.image} alt={cocktail.name} />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{cocktail.name}</h3>
+        <h4>{cocktail.glass}</h4>
+        <p>{cocktail.info}</p>
+        <Link to={`/cocktail/${cocktail.id}`} className='btn btn-primary btn-details'>
+        DETAILS
+        </Link>
+      </div>
+    </article>
   )
 }
 
